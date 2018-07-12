b5f55da640ecd624da770b7e0f0a3937ec16c6d1
zhongming <zhongming4762@hotmail.com>
2018-07-12 15:59:49 +0800

    1、上传前端插件包 2、上传rest请求工具类

---
+++ cloud_web/cloud_ui_util/src/main/resources/static/js/util/RestUtil.ts
@@ -0,0 +1,87 @@
+/**
+ * rest工具类
+ */
+declare let $;
+declare let layer;
+
+namespace com.clouddo.ui.util {
+    export class RestUtil {
+        /**
+         * 发送ajax请求，异步
+         * @param {string} url 请求URL
+         * @param {Object} parameterSet 请求参数
+         * @param {Function} success 请求成功回调函数
+         * @param {Function} error 请求失败回调函数
+         * @param successCallbackParameters 请求成功参数（可变参数）
+         */
+        public static postAjax(url : string, parameterSet : Object, success : Function, error : Function, ...successCallbackParameters : any[]) {
+            RestUtil.postAjaxBase(true, url , parameterSet, success, error, ...successCallbackParameters);
+        }
+        /**
+         * 发送ajax请求，同步
+         * @param {string} url 请求URL
+         * @param {Object} parameterSet 请求参数
+         * @param {Function} success 请求成功回调函数
+         * @param {Function} error 请求失败回调函数
+         * @param successCallbackParameters 请求成功参数（可变参数）
+         */
+        public static postAjaxAsync(url : string, parameterSet : Object, success : Function, error : Function, ...successCallbackParameters : any[]) {
+            RestUtil.postAjaxBase(false, url , parameterSet, success, error, ...successCallbackParameters);
+        }
+
+
+        /**
+         * 发送ajax请求，异步
+         * @param async ture同步  false异步
+         * @param {string} url 请求URL
+         * @param {Object} parameterSet 请求参数
+         * @param {Function} success 请求成功回调函数
+         * @param {Function} error 请求失败回调函数
+         * @param successCallbackParameters 请求成功参数（可变参数）
+         */
+        public static postAjaxBase(async : boolean ,url : string, parameterSet : Object, success : Function, error : Function, ...successCallbackParameters : any[]) {
+            $.ajax({
+                type: "post",
+                url : localStorage.getItem("baseUrl") + url,
+                dataType:"json",
+                async : async,
+                contentType : 'application/json; charset=UTF-8',
+                // data : JSON.stringify、(parameterSet || {}),
+                data : JSON.stringify(parameterSet || {}),
+                success : function (data) {
+                    RestUtil.defalutSuccessFunction(data, success, error, ...successCallbackParameters);
+                },
+                error : function (data) {
+                    RestUtil.defalutErrorFunction(data, error);
+                },
+                beforeSend : function (request) {
+                    request.setRequestHeader("Authorization", localStorage.getItem("Authorization"));
+                }
+            });
+        }
+
+
+        private static defalutSuccessFunction(data : any, success : Function, error : Function, ...successCallbackParameters) {
+            if (data.code == "SUCCESS") {
+                if (success != null) {
+                    //执行回调函数
+                    success(data.data,  ...successCallbackParameters);
+                } else {
+                    //没有请求成功回调，则打印请求数据
+                    console.log(data);
+                }
+            } else {
+                RestUtil.defalutErrorFunction(data, error);
+            }
+        }
+
+        private static defalutErrorFunction(data : any, error : Function) {
+            if (error != null) {
+                error(data);
+            } else {
+                console.log(data);
+                layer.alert('数据获取异常！', {icon: 0});
+            }
+        }
+    }
+}
