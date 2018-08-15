20c917c6095fc3731cee868a87d8ac0fea17cc29
zhongming <zhongming4762@hotmail.com>
2018-08-15 09:03:02 +0800

    添加启动类

---
+++ cloud_news/cloud_news_server/src/main/java/com/clouddo/news/server/CloudNewsServerApplication.java
@@ -0,0 +1,29 @@
+package com.clouddo.news.server;
+
+import com.cloudd.commons.auth.config.AuthCommonImport;
+import com.clouddo.log.common.config.LogCommonImport;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 新闻服务启动类
+ * @author zhongming
+ */
+@SpringBootApplication
+@EnableEurekaClient   //服务提供者
+@EnableFeignClients        //开启feign
+@Import({
+		//认证工具包引入
+		AuthCommonImport.class,
+		//引入日志工具包
+		LogCommonImport.class
+})
+public class CloudNewsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNewsServerApplication.class, args);
+	}
+}
